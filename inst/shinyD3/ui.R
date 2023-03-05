22e57d912cbfecd70c00c897369a8ee33534867c
kmezhoud <kmezhoud@gmail.com>
2023-03-05 15:36:15 +0100

    reset app

--- inst/shinyD3/ui.R
+++ inst/shinyD3/ui.R
@@ -13,21 +13,19 @@ library(shiny)
 fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("D3 and R, a match made in heaven"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+          
+          
+          
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+
         )
     )
 )
